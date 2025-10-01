755f1913752457e1168900e136bb05322419f137
pandarudra <rudrapanda8206@gmail.com>
2025-10-01 17:38:00 +0530

    redis fixed

--- server/config/redis.js
+++ server/config/redis.js
@@ -2,31 +2,22 @@ import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
 
+const redisCredentials = {
+  port: Number(process.env.REDIS_PORT),
+  host: process.env.REDIS_HOST,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+};
+
+// redis client
 export async function createRedisClient() {
-  const client = new Redis({
-    port: Number(process.env.REDIS_PORT),
-    host: process.env.REDIS_HOST,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-  });
+  const client = new Redis(redisCredentials);
   return client;
 }
 
-/**
- * Create pub/sub clients for Socket.IO adapter.
- */
+// pub/sub clients for socket.io-redis adapter
 export async function createAdapterClients() {
-  const pubClient = new Redis({
-    port: Number(process.env.REDIS_PORT),
-    host: process.env.REDIS_HOST,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-  });
-  const subClient = new Redis({
-    port: Number(process.env.REDIS_PORT),
-    host: process.env.REDIS_HOST,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-  });
+  const pubClient = new Redis(redisCredentials);
+  const subClient = new Redis(redisCredentials);
   return { pubClient, subClient };
 }
